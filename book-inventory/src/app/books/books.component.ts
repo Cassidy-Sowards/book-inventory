d4f5f8f929fcc90b2f5a11af6cc9d31fce5ceafb
Cassidy-Sowards <crsowards@outlook.com>
2023-02-27 18:17:57 -0500

    Set up new service for managing books

--- book-inventory/src/app/books/books.component.ts
+++ book-inventory/src/app/books/books.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../services/book.service';
 
 import { Book } from '../interfaces/book';
 
-import { BOOKS } from '../mock-books';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
 
-  books = BOOKS;
+  books: Book[] = [];
   selectedBook?: Book;
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.books = this.bookService.getBooks();
+  }
 
   onSelect(book: Book){
     this.selectedBook = book;
   }
+
+
 
 
 }
